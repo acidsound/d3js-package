f2c2491e3b0ec665fd373edebce24f3661052dab
lfergon <lfergon@gmail.com>
2013-08-17 15:02:37 +0200

    Works with meteor 0.6.5

--- package.js
+++ package.js
@@ -3,5 +3,9 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
+  if(api.export){
+    api.use('d3', 'client');
+    api.export('d3', 'client');
+  }
   api.add_files('lib/d3/d3.v2.min.js', 'client');
 });
